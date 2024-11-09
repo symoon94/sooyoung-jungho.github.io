10ce5e921dcca54ffe60837eaeaaf4742df17040
Sooyoung Moon <msy0128@gmail.com>
2024-11-10 06:48:12 +0900

    edit env

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     unoptimized: true,
   },
   env: {
-    NEXT_PUBLIC_GITHUB_TOKEN: process.env.GITHUB_TOKEN
+    NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN
   },
 };
 

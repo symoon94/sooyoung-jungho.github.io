82f65fabe996419c58c2a92c15719ee2912d0513
Sooyoung Moon <msy0128@gmail.com>
2024-11-12 11:02:13 +0900

    redirect

--- app/components/RedirectComponent.tsx
+++ app/components/RedirectComponent.tsx
@@ -3,11 +3,24 @@
 import { useEffect } from 'react';
 
 export default function RedirectComponent() {
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.location.hostname === 'sooyoung-jungho.github.io') {
-      window.location.href = 'https://sooyoung-jungho.netlify.app' + window.location.pathname;
-    }
-  }, []);
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const hostname = window.location.hostname;
+            if (hostname === 'sooyoung-jungho.github.io') {
+                // 즉시 리다이렉션을 위해 document.write 사용
+                document.write(`
+          <html>
+            <head>
+              <meta http-equiv="refresh" content="0; url=https://sooyoung-jungho.netlify.app${window.location.pathname}">
+            </head>
+            <body>
+              <p></p>
+            </body>
+          </html>
+        `);
+            }
+        }
+    }, []);
 
-  return null;
+    return null;
 } 
